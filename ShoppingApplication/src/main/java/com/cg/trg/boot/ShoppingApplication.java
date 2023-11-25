cbacfdea5278ab86b433a45565107fd5b22b7609
nejamddin8944 <nejamuddin.ali@capgemini.com>
2023-11-25 12:12:14 +0530

    added

---
+++ ShoppingApplication/src/main/java/com/cg/trg/boot/ShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.cg.trg.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingApplication.class, args);
+	}
+
+}
