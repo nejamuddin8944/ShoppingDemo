f8a44349b29ca844c7283863c41267f0352b6c9b
vikash73200 <vikashojha732000@gmail.com>
2023-11-25 09:58:41 +0530

    abc2

--- SimpleControllerProject/simpleController/src/main/java/com/cg/trg/boot/SimpleControllerApplication.java
+++ SimpleControllerProject/simpleController/src/main/java/com/cg/trg/boot/SimpleControllerApplication.java
@@ -8,6 +8,7 @@ public class SimpleControllerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleControllerApplication.class, args);
+		System.out.println("project123");
 	}
 
 }
