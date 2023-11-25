dc90773283f2ab7a8af5ce5b0d4cb90bc47f2efc
nejamddin8944 <nejamuddin.ali@capgemini.com>
2023-11-25 09:38:17 +0530

    comment in Controller

--- SimpleControllerProject/simpleController/src/main/java/com/cg/trg/boot/SimpleController.java
+++ SimpleControllerProject/simpleController/src/main/java/com/cg/trg/boot/SimpleController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+//this changed is made by Nejam
 
 @Controller
 @RequestMapping("")
