91e21710752d7381c0063e98fdd12c5b55cea40b
nejamddin8944 <nejamuddin.ali@capgemini.com>
2023-11-25 09:29:49 +0530

    project added

---
+++ SimpleControllerProject/SecurityProject/src/test/java/com/cg/trg/boot/SecurityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.trg.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ SimpleControllerProject/simpleController/src/main/java/com/cg/trg/boot/SimpleController.java
@@ -0,0 +1,35 @@
+package com.cg.trg.boot;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("")
+public class SimpleController {
+//	@RequestMapping(method = RequestMethod.GET)
+	@GetMapping("/welcome")
+	public String welcome(Model model) {
+		model.addAttribute("message","Welcome to Spring Boot Application");
+		
+		return "response";
+	}
+	
+	@GetMapping("/admin")
+	public String getAdmin(Model model) {
+		model.addAttribute("message","This is admin page");
+		
+		return "response";
+	}
+	
+	@GetMapping("/user")
+	public String getUser(Model model) {
+		model.addAttribute("message","This is user page");
+		
+		return "response";
+	}
+	
+}
