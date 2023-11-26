0ebaeb8aeeff0d8c1933ba8eccbe12a21e2f987d
nejamddin8944 <nejamuddin.ali@capgemini.com>
2023-11-26 10:52:24 +0530

    hello

--- ShoppingApplication/src/main/java/com/cg/trg/boot/DbInitForAllEntity.java
+++ ShoppingApplication/src/main/java/com/cg/trg/boot/DbInitForAllEntity.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
 import com.cg.trg.boot.dto.Address;
+import com.cg.trg.boot.dto.Category;
 import com.cg.trg.boot.repositories.AddressRepository;
 import com.cg.trg.boot.repositories.CartRepositories;
 import com.cg.trg.boot.repositories.CategoryRepositories;
@@ -31,6 +32,10 @@ public class DbInitForAllEntity implements CommandLineRunner{
 		//Adding Data to Address Table
 		addRepo.save(new Address(10, "MG-21", "GAR-Infobahn", "Hyderabad", "Telangana", "India", "720001"));
 		addRepo.save(new Address(20, "MG-22", "GAR-Infobahn", "Kolkata", "West Bengal", "India", "70001"));
+		
+		//Adding Data to Category Table
+		catRepo.save(new Category("Z-602", "Electronics"));
+		catRepo.save(new Category("Z-801", "Clothing"));
 		
 	}
 
--- ShoppingApplication/src/main/java/com/cg/trg/boot/dto/Category.java
+++ ShoppingApplication/src/main/java/com/cg/trg/boot/dto/Category.java
@@ -3,37 +3,41 @@ package com.cg.trg.boot.dto;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
-
 @Entity
 public class Category {
 	@Id
 	private String catId;
-	private  String catagoryName;
+	private String catagoryName;
+
 	public Category() {
 		super();
 	}
+
 	public Category(String catId, String catagoryName) {
 		super();
 		this.catId = catId;
 		this.catagoryName = catagoryName;
 	}
+
 	public String getCatId() {
 		return catId;
 	}
+
 	public void setCatId(String catId) {
 		this.catId = catId;
 	}
+
 	public String getCatagoryName() {
 		return catagoryName;
 	}
+
 	public void setCatagoryName(String catagoryName) {
 		this.catagoryName = catagoryName;
 	}
+
 	@Override
 	public String toString() {
 		return "Catagory [catId=" + catId + ", catagoryName=" + catagoryName + "]";
 	}
-	
-	
 
 }
